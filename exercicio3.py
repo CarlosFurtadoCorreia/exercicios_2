7f7307d14cc34f5a700226a9f4c3a7fd567c0bc5
Carlos <carlosfurtadocorreia@enta.pt>
2022-09-28 23:14:13 +0000

    exercicios 1, 2, 3 completados

--- exercicio3.py
+++ exercicio3.py
@@ -6,22 +6,43 @@
 - Frase tem que terminar obrigatoriamente com '. ! ?'''
 
 if __name__ == '__main__':
-    frase = input('Insira uma frase com + de 10 caracteres. (Deve terminar com ". ! ?"): ')
-    ver_char = [frase]
-    count_char_iguais = []
     count_frase = 0
-    fim = ['.', '!', '?']
-    for f in frase:
-        count_frase += 1
-    print({count_frase})
-    if count_frase < 10:
-        print('A sua frase tem que ter + de 10 caracteres.')
-    else:
-        pass
-    #for c in range(frase):
+    final_frase = ['.', '!', '?']
 
-    '''if len(frase - 1) != fim:
-        pass
-    else:
-        print('A sua frase deve terminar com ". ! ?"')
-    print('passou')'''
+    while count_frase < 10:
+        frase = input('Insira uma frase com mais de 10 caracteres. (Deve terminar com ". ! ?"): ')
+        count_frase = len(frase)
+        x = count_frase - 1
+        if count_frase < 10:
+            print('A frase deve ter mais de 10 caracteres.')
+
+        if frase[x] in final_frase:
+            pass
+        else:
+            print('A frase deve terminar com ". ! ?"')
+            x = 0
+
+        #NÃO ESTÁ FUNCIONADO A PARTIR DAQUI
+        check_char1 = 0
+        check_char2 = 0
+        check_char3 = 0
+
+        for j in range(0, len(frase)):
+            if j + 2 < len(frase):
+                check_char1 = frase[j]
+                check_char2 = frase[j + 1]
+                check_char3 = frase[j + 2]
+                if check_char1 == check_char2 and check_char1 == check_char3:
+                    print(f'A frase não pode ter mais que 2 caracteres repetidos seguidos.'
+                          f'({check_char1}'
+                          f'{check_char2}'
+                          f'{check_char3})')
+        print(f'{frase}')
+
+        '''for j in range(1, len(frase)):
+            if check_char == frase[j]:
+                print('A frase não pode ter mais que 2 caracteres repetidos seguidos.')
+                x = 0
+                break
+            check_char = frase[j]
+        print(frase)'''
